21b1a5ca96a2683b909587fbb847edf2e7dcfa45
YagoCastello <yagodealmeidacs@gmail.com>
2023-08-12 17:13:09 -0300

    ex 1-14

---
+++ 1-14.py
@@ -0,0 +1,6 @@
+# As aulas apresentadas da aula 1 a 14, se trata basicamente da apresentação do curso 
+# E a preparação do computador para aprender python
+
+
+# The classes presented from class 1 to 14 are basically the presentation of the course
+# And preparing the computer to learn python
