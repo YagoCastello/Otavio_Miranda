454fb124e60636e09c24f3e81cbf7619959c6d13
Yago Castello Saloman <104936515+YagoCastello@users.noreply.github.com>
2024-05-15 19:26:14 -0300

    Dia_15_maio_2024
    a

---
+++ Aula_17.py
@@ -0,0 +1,11 @@
+#Docstring ,serve para comentar várias linhas. 
+# E fazer a documentação de Func
+
+"""
+
+bala
+coco
+
+
+"""
+print("teste")
---
+++ Aula_18.py
@@ -0,0 +1,4 @@
+print(12,54,sep="-",end='##\n')
+print(12,54,sep="",end='11111111111444')
+print(65,788,sep='zebra',end='\n$$$$$$')
+print(32,547,sep=' zebra ',end="bola")
