34ce7ad84afb439830a4ce229bcaa1cef0cd2b6c
Yago Castello Saloman <104936515+YagoCastello@users.noreply.github.com>
2024-06-27 22:24:31 -0300

    dia_27_06
    novos scripts

---
+++ Aula_24.py
@@ -0,0 +1,12 @@
+nome ='yago'
+sobrenome = 'saloman'
+idade = 27
+ano_nascimento = 1997
+maior_de_idade =27>18
+altura_metros =1.75
+
+print('nome = ',nome)
+print('sobrenome = ',sobrenome)
+print('idade = ', idade)
+print('É maior de idade? ',maior_de_idade)
+print('Altura em metros: ',altura_metros)
