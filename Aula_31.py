8a7a1867e8e1c17a076d774ed0fccb5e8b4ee387
Yago Castello Saloman <104936515+YagoCastello@users.noreply.github.com>
2024-07-25 23:39:58 -0300

    b

---
+++ Aula_31.py
@@ -0,0 +1,13 @@
+nome = 'Pedro'
+altura = 1.90
+peso = 102
+imc = peso / altura ** 2
+
+"f-strings"
+linha_1 = f'{nome} tem {altura:.2f} de altura,'
+linha_2 = f'pesa {peso} quilos e seu imc é'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
