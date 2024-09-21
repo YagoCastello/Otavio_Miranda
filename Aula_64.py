e561105d4c15b7bf845a12c456d8386142410313
Yago Castello <104936515+YagoCastello@users.noreply.github.com>
2024-09-21 15:49:32 -0300

    20_09_2024
    arquivos criados sobre loop

---
+++ Aula_64.py
@@ -0,0 +1,13 @@
+"""
+Iterando strings com while
+"""
+#       012345678910
+nome = 'Luiz Otávio'  # Iteráveis
+#      1110987654321
+tamanho_nome = len(nome)
+print(nome)
+print(tamanho_nome)
+print(nome[3])
+
+nova_string = ''
+nova_string += '*Y*a*g*o* *C*a*s*t*e*l*l*o'
