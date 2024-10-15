ad0511e54fd4f497f4cda41e3b1ee60a89303613
Yago Castello <104936515+YagoCastello@users.noreply.github.com>
2024-10-15 18:29:18 -0300

    15-10-2024

---
+++ Aula_115.py
@@ -0,0 +1,10 @@
+"""
+    
+    
+    Academicamente, os termos Higher Order Functions e First-Class Functions têm significados diferentes.
+
+Higher Order Functions - Funções que podem receber e/ou retornar outras funções
+
+First-Class Functions - Funções que são tratadas como outros tipos de dados comuns (strings, inteiros, etc...)
+    
+"""
