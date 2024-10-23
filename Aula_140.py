051660ce1609c4abba4968bdec113c310674f2df
Yago Castello <yagodealmeidacs@gmail.com>
2024-10-23 11:31:13 -0300

    23-10-2024

--- Aula_140.py
+++ Aula_140.py
@@ -1,52 +1,94 @@
-# Sets - Conjuntos em Python (tipo set)
-# Conjuntos são ensinados na matemática
-# https://brasilescola.uol.com.br/matematica/conjunto.htm
-# Representados graficamente pelo diagrama de Venn
-# Sets em Python são mutáveis, porém aceitam apenas
-# tipos imutáveis como valor interno.
+# numeros = [1,2,3,4,5,6,7,8]
+# divisao = [ numero/2 for numero in numeros]
+# print(numeros)
+# print(divisao)
 
-# Criando um set
-# set(iterável) ou {1, 2, 3}
-# s1 = set('Luiz')
-# s1 = set()  # vazio
-# s1 = {'Luiz', 1, 2, 3}  # com dados
+########MAP##########################
+# numeros = [1,2,3,4,5,6,7,8]
 
-# Sets são eficientes para remover valores duplicados
-# de iteráveis.
-# - Seus valores serão sempre únicos;
-# - Não aceitam valores mutáveis;
-# - não tem índexes;
-# - não garantem ordem;
-# - são iteráveis (for, in, not in)
-# l1 = [1, 2, 3, 3, 3, 3, 3, 1]
-# s1 = set(l1)
-# l2 = list(s1)
-# s1 = {1, 2, 3}
-# print(3 not in s1)
-# for numero in s1:
-#     print(numero)
+# def divisãoFn(x,y):
+#     return x/y
 
-# Métodos úteis:
-# add, update, clear, discard
-s1 = set()
-s1.add('Luiz')
-s1.add(1)
-s1.update(('Olá mundo', 1, 2, 3, 4))
-# s1.clear()
-s1.discard('Olá mundo')
-s1.discard('Luiz')
-print(s1)
-# print(s1)
 
-# Operadores úteis:
-# união | união (union) - Une
-# intersecção & (intersection) - Itens presentes em ambos
-# diferença - Itens presentes apenas no set da esquerda
-# diferença simétrica ^ - Itens que não estão em ambos
-s1 = {1, 2, 3}
-s2 = {2, 3, 4}
-s3 = s1 | s2
-s3 = s1 & s2
-s3 = s2 - s1
-s3 = s1 ^ s2
-print(s3)
+# def multiplicaçãoFn(x,y):
+#     return x*y
+
+
+# def potenciaçãoFn(x,y):
+#     return x**y
+
+# divisao = [ divisãoFn(numero,2) for numero in numeros]
+# multiplicacao = [ multiplicaçãoFn(numero,2) for numero in numeros]
+# potencializacao = [ potenciaçãoFn(numero,2) for numero in numeros]
+# print('Exemplo de MAP')
+# print(numeros)
+# print(divisao)
+# print(multiplicacao)
+# print(potencializacao)
+
+# #FILTER###################
+
+# valores = [1,2,3,4,5,6,7,8,9,10]
+
+# novos_valores = [valor for valor in valores if valor >5]
+# print('Exemplo de filter')
+# print(novos_valores)
+# impares = [valor for valor in valores if valor%2==1]
+# print('Valores ímpares',impares)
+
+# pares = [valor for valor in valores if valor%2==0]
+# print('Valores ímpares',pares)
+
+# numeros = [1,2,3,4,5,6,7,8,9,10]
+
+# outro_if = [
+
+#     numero if numero != 6
+#     else 600
+#     for numero in numeros
+#     if numero %2 ==0 
+# ]
+
+# print(outro_if)
+
+# numeros = [1,2,3,4,5,6,7,8,9,10]
+
+# linhas_e_colunas = [ 
+#     (x,y)
+#     if y != 2 else 'Você não é 2'
+#     for x in range(1,11)
+#     for y in range(1,6)
+#     if x != 2
+# ]
+
+# print(' ')
+# print(' ')
+# print(' ')
+# print(' ')
+# print(' ')
+# linhas_e_colunas_multiplicacao = [ 
+#     (x,y)
+#     if y != 2 else (x*75,y*1000)
+#     for x in range(1,11)
+#     for y in range(1,6)
+#     if x != 2
+# ]
+# print(linhas_e_colunas_multiplicacao)
+
+
+# string = 'Otávio Miranda'
+# numero_de_letras = 2
+# nova_string = '.'.join([
+#     string[indice:indice +  numero_de_letras]
+#     for indice in range(0, len(string), numero_de_letras)
+#     ])
+# print(nova_string)
+
+nomes = ['Otavio','Felipe','Yago','Maria']
+
+# novos_nomes = [nome.lower() for nome in nomes]
+# print(novos_nomes)
+bugado = [
+    f'{nome[:-1].lower()}{nome[-1].upper()}'
+    for nome in nomes ]
+print(bugado)
