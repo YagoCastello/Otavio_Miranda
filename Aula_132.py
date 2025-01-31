27b282e41a313418129018967bf4b1d4409b424f
Yago Castello <yagodealmeidacs@gmail.com>
2025-01-31 09:35:58 -0300

    att

--- Aula_132.py
+++ Aula_132.py
@@ -45,4 +45,11 @@ for lista in lista_de_listas_de_inteiros:
         print('Não tem termos repetidos!')
     print('-='*20)
 
-            
+
+###
+lista = ['nasa','pato','casa','bolo','nasa','pato','casa','bolo']
+meu_SET = {}
+for item in lista:
+    if item not in meu_SET:
+        meu_SET.update(item)
+        print(item)
